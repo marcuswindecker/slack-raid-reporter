99578ddbb064d9a1c0c6e8aaeafd47edc4123857
Marcus Windecker <marcus@onxmaps.com>
2018-03-08 17:37:49 -0700

    Removing phantomjs, returning link to raid.report for now

--- public/index.php
+++ public/index.php
@@ -2,31 +2,18 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-use JonnyW\PhantomJs\Client;
-
 require '../vendor/autoload.php';
-require '../src/classes/Upload.php';
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
 $app = new \Slim\App(['settings' => $config]);
-$app->get('/stats', function (Request $request, Response $response, array $args) {
-    $upload = new Upload();
+$app->post('/stats', function (Request $request, Response $response, array $args) {
 
-    $phantomClient = Client::getInstance();
-    $phantomClient->getEngine()->setPath(dirname(__FILE__).'/bin/phantomjs');
-    $phantomClient->getEngine()->debug(true);
+    $requestBody = $request->getParsedBody();
 
-    $phantomRequest  = $phantomClient->getMessageFactory()->createRequest('https://google.com', 'GET', 5000);
-    $phantomResponse = $phantomClient->getMessageFactory()->createResponse();
-
-    $phantomClient->send($phantomRequest, $phantomResponse);
-
-    // $requestBody = $request->getParsedBody();
-
-    // $responseBody['response_type'] = 'in_channel';
-    // $responseBody['text'] = $requestBody['text'];
+    $responseBody['response_type'] = 'in_channel';
+    $responseBody['text'] = 'Here\'s the raid report for ' . $requestBody['text'] . ': https://raid.report/ps/' . $requestBody['text'];
     // $responseBody['attachments'] = [
     // 	[
     // 		'color' => 'good',
@@ -35,12 +22,10 @@ $app->get('/stats', function (Request $request, Response $response, array $args)
     // 	]
     // ];
 
-    // $response = $response->withStatus(200)
-    //   ->withHeader('Content-Type', 'application/json')
-    //   ->write(json_encode($responseBody));
+    $response = $response->withStatus(200)
+      ->withHeader('Content-Type', 'application/json')
+      ->write(json_encode($responseBody));
 
-    $log = $phantomClient->getLog();
-
-    echo "<pre>" . json_encode($log, JSON_PRETTY_PRINT) . "</pre>";
+    return $response;
 });
 $app->run();
